b22684c1d35cbf377633d030ffd232fb4ff9dc44
Izzy Deane <github@oceanity.net>
2024-12-21 17:39:01 -0600

    Cleaned up definitions and fixed outputs

--- src/utils/mastodon-helpers.ts
+++ src/utils/mastodon-helpers.ts
@@ -1,3 +1,5 @@
+import { logger } from "@oceanity/firebot-helpers/firebot";
+import { convert } from "html-to-text";
 import { Entity } from "megalodon";
 import { MASTODON_AUTHOR_VARIABLE_PREFIX } from "../constants";
 
@@ -9,14 +11,19 @@ export const getUserProfileMetadata = (
   [`${prefix}Username`]: profile.username,
   [`${prefix}DisplayName`]: profile.display_name,
   [`${prefix}AvatarUrl`]: profile.avatar,
-  [`${prefix}Bio`]: profile.note,
+  [`${prefix}BioHtml`]: replaceEmojisInHtml(profile.note, profile.emojis),
+  [`${prefix}Bio`]: convert(profile.note, {
+    selectors: [{ selector: "a", options: { ignoreHref: true } }],
+  }),
   [`${prefix}BannerUrl`]: profile.header,
   [`${prefix}Id`]: profile.id,
   [`${prefix}CreatedAt`]: profile.created_at,
 });
 
 export const getPostMetadata = (post: Entity.Status, prefix: string) => ({
-  [`${prefix}Text`]: post.plain_content,
+  [`${prefix}Text`]: convert(post.content, {
+    selectors: [{ selector: "a", options: { ignoreHref: true } }],
+  }),
   [`${prefix}Html`]: replaceEmojisInHtml(post.content, post.emojis),
   [`${prefix}Uri`]: post.uri,
   [`${prefix}Url`]: post.url,
@@ -28,9 +35,10 @@ export const getPostMetadata = (post: Entity.Status, prefix: string) => ({
 });
 
 export function replaceEmojisInHtml(html: string, emojis: Array<Entity.Emoji>) {
+  logger.info(html + `\n${JSON.stringify(emojis)}`);
   for (const emoji of emojis) {
     html = html.replace(
-      `/:${emoji.shortcode}:/g`,
+      new RegExp(`:${emoji.shortcode}:`, "g"),
       `<img class="emoji" alt="${emoji.shortcode}" src="${emoji.url}" />`
     );
   }
